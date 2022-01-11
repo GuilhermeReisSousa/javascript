77d0ff8ba8fb306f7f784e980270be3ba43f6881
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-01-11 12:23:39 -0300

    ex

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do do caro é ${vel} Km/h`)
+if (vel > 60) { // Condição simples 
+    console.log('Você ultaprassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
