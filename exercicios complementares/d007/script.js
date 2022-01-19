ed6fdef90d08f69f6476eabcfbffdd03612372b0
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-01-19 13:43:38 -0300

    d007 complete

--- exercicios complementares/d007/script.js
+++ exercicios complementares/d007/script.js
@@ -1,4 +1,13 @@
-function carregar(){
+var cota = Number(prompt("Qual a cotação atual do dolar?"))
+if(cota == 0){
+    alert("[ERRO] Reecaregue a página porfavor eVerifique os dados.")
+}
+function fnc1(){
+    var real = Number(prompt("Quanto você tem na sua carteira atual (em reais)"))
+    var conver = Number((real / cota)).toFixed(2)
     var res = document.querySelector("#res")
-    res.innerHTML = "Testando o innerHTML"
+    res.innerHTML = `<p>Se você tem R$${real} na sua carteira, você pode conseguir ${conver} dolares americanos</p>`
+    if(real == 1 ){
+        res.innerHTML = `<p>Se você tem R$${real} na sua carteira, você pode conseguir R$0 dolares americanos</p>`
+    }
 }
