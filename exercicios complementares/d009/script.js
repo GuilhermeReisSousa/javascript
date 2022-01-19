fdea02e64b3dbcf0785558fdd129489c4df23790
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-01-19 20:14:54 -0300

    d009

---
+++ exercicios complementares/d009/script.js
@@ -0,0 +1,22 @@
+// salario atual * reajuste / 100 = funciona
+// var titulo = document.querySelector('div#titulo').innerHTML = `<h2>${nome} recebeu um aumento salárial</h2>`
+// var atual = document.querySelector('div#atual').innerHTML = `<p>O salário atual era R$${sala}</p>`
+// var aumento = document.querySelector('div#aumento').innerHTML = `<p>Com um aumento de ${porcen}%, o salário vai aumentar R$${conta} no próximo mês.</p>`
+// var res = document.querySelector('div#res').innerHTML = `<p>E a partir daí, ${nome} vai passar a ganhar ${res}</p>`
+
+
+function fnc1(){
+    var nome = prompt('Qual é o nome do funcionário?')
+    var sala = Number(prompt(`Qual é o salário de ${nome}`)).toFixed(2)
+    var porcen = Number(prompt(`O salário de ${nome} vai ser reajustado em qual procentagem?`))
+    var conta = ((sala * porcen) / 100).toFixed(2)
+    var res = (conta + 1)
+
+    var titulo = document.querySelector('div#titulo').innerHTML = `<h2>${nome} recebeu um aumento salárial</h2>`
+
+    var atual = document.querySelector('div#atual').innerHTML = `<p>O salário atual era R$${sala}</p>`
+
+    var aumento = document.querySelector('div#aumento').innerHTML = `<p>Com um aumento de ${porcen}%, o salário vai aumentar R$${conta} no próximo mês.</p>`
+    
+    var res = document.querySelector('div#res').innerHTML = `<p>E a partir daí, ${nome} vai passar a ganhar ${res}</p>`
+}
