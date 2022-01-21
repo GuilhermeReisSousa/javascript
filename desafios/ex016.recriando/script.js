cff8b38e38f042e91ae04f7f78435a5833d77fa9
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-01-20 23:13:37 -0300

    ex016 recriando

---
+++ desafios/ex016.recriando/script.js
@@ -0,0 +1,29 @@
+function contar(){
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var contagem = document.getElementById('contagem')
+
+    if(inicio.value == 0 || fim.value == 0 || passo.value == 0){
+        alert('[ERRO] Confira os dados e tente novamente')
+        contagem.innerHTML = `Impossível contar`
+    }else{
+        contagem.innerHTML = 'Contando: <br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number (passo.value)
+        if( i < f){
+            //CRESCENTE
+            for(var c = i; c <= f; c += p){
+                contagem.innerHTML += `${c} \u{1F449}`
+            }  
+        }else{
+            //REGRESSIVA
+            for(var c = i; c >= f; c -= p){
+                contagem.innerHTML += `\u{1F449}`
+            }
+        }
+        contagem.innerHTML += `\u{1F3C1}`
+    }
+
+}
