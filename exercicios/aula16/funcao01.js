90fb2e86b0aeba85be50ae632ebfe547362bfe23
Guilherme Reis Sousa <96916435+GuilhermeReisSousa@users.noreply.github.com>
2022-01-24 22:27:03 -0300

    funções

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parimp(n){
+    if(n%2 == 0){
+        return 'Par'
+    }else {
+        return 'Ímpar'
+    }
+}
+console.log(parimp(223))
+/* 
+var res = parimp(2)
+console.log(res)
+ */
